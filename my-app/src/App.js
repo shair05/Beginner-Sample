26b9a17161a3faaf6203ef202663f46989d4823f
Shair Ali <shair.kmscs@gmail.com>
2023-11-06 21:04:05 +0500

    First Commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h2 style={{color:"blue"}}>I am learning React.js from Mr. Wajahat Ali.</h2>
+    </div>
+  );
+}
+
+export default App;
