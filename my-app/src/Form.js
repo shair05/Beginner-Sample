2e6905eb8a06e044d22c991be74c940418703589
Shair Ali <shair.kmscs@gmail.com>
2023-11-22 19:56:54 +0500

    Update Form.js
    Add a text field

--- my-app/src/Form.js
+++ my-app/src/Form.js
@@ -1,12 +1,36 @@
-import React from "react"
+import React from "react";
+/*import ReactDOM from 'react-dom/client';*/
+import { useState } from 'react';
  
 
 function Form() {
+
+    //To handle inputs
+    const [inputs, setInputs] = useState({});
+   
+   //Setting Input field value
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))//do not understand
+      }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(inputs);
+      }
+
     return(
-    <div className="text-center container">
-        <h3>REGISTRATION FORM IN REACT</h3>
-        <form className="mx-2">
-            <input type="text" name="Name" id="Name" value="" placeholder="Enter Name..." className="form-control" />
+    <div className="text-center container w-50">
+        <h3>FORM ELEMENTS IN REACT</h3>
+        
+        <form onSubmit={handleSubmit} className="mx-2">
+            <div className="form-row">
+                <div className="form-group col text-start">
+                    <label className="mb-0" for="FullName"><b><span style={{color:'red'}}>*</span>Full Name</b></label>
+                    <input type="text" className="form-control" name="FullName" id="FullName" value={inputs.FullName || ""} onChange={handleChange} placeholder="Enter Name..." />
+                </div>
+            </div>   
         </form>
     </div>
    )  
