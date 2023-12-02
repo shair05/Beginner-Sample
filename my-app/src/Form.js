c1d7d80e4f80b0a705c1853264051ab074766f4e
Shair Ali <shair.kmscs@gmail.com>
2023-12-02 20:22:53 +0500

    Update Form.js
    button added

--- my-app/src/Form.js
+++ my-app/src/Form.js
@@ -5,10 +5,10 @@ import { useState } from 'react';
 
 function Form() {
 
-    //To handle inputs
+    //To handle inputs values through an empty object
     const [inputs, setInputs] = useState({});
    
-   //Setting Input field value
+   //Setting Input field value and storing in an object as inputs
     const handleChange = (event) => {
         const name = event.target.name;
         const value = event.target.value;
@@ -17,6 +17,7 @@ function Form() {
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        //to convert object to array --> const myArray = Object.values(inputs);
         alert(inputs);
       }
 
@@ -24,13 +25,23 @@ function Form() {
     <div className="text-center container w-50">
         <h3>FORM ELEMENTS IN REACT</h3>
         
-        <form onSubmit={handleSubmit} className="mx-2">
-            <div className="form-row">
-                <div className="form-group col text-start">
+        <form onSubmit={handleSubmit} className="mx-2 d-grid gap-3">
+            <div className="">
+                <div className="text-start">
                     <label className="mb-0" for="FullName"><b><span style={{color:'red'}}>*</span>Full Name</b></label>
                     <input type="text" className="form-control" name="FullName" id="FullName" value={inputs.FullName || ""} onChange={handleChange} placeholder="Enter Name..." />
                 </div>
-            </div>   
+            </div> 
+
+            <div className="">
+                {/*<div className="d-inline me-2">
+                    <button className="btn btn-primary" type="reset" name="Reset" id="Reset">Reset</button>
+                   </div>*/}
+                <div className="d-inline">
+                    <button className="w-100 btn btn-success" type="submit" name="Submit" id="Submit">Submit</button> 
+                </div>
+            </div>
+
         </form>
     </div>
    )  
